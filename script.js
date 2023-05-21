fcac62bb96706536403a471081571f7724eafcc4
Yashido <jbpablico27@gmail.com>
2023-05-20 20:17:58 -0600

    Book constructor

---
+++ script.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(author, title, pages, progress) {
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.progress = progress
+}
+
+function addBookToLibrary() {
+
+}
