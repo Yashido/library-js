cef6e15c4ab5d17624dc4f20aae71aeff1e23b93
wimllie <jbpablico27@gmail.com>
2023-05-28 17:26:09 -0500

    Confused

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 let form = document.getElementsByClassName("book-submition");
-let author = document.getElementById("book-title");
-let title = document.getElementById("book-title");
-let pages = document.getElementById("book-pages");
-let progress = document.getElementById("book-progress");
+let bookTitle = document.getElementById("book-title").value;
+let bookAuthor = document.getElementById("book-author").value;
+let bookPages = document.getElementById("book-pages").value;
+let bookProgress = document.getElementById("book-progress").value;
 
+console.log(bookAuthor);
 
 let myLibrary = [];
 
-function Book(title, author, pages, progress) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.progress = progress
+function Book(bookTitle, bookAuthor, bookPages, bookProgress) {
+    this.bookTitle = bookTitle,
+    this.bookAuthor = bookAuthor,
+    this.bookPages = bookPages,
+    this.bookProgress = bookProgress
 };
 
+/*
+let x = 0;
+while(x != 9999999999999) {
+    console.log(author);
+}
+*/
 
-function addBookToLibrary() {
 
-};
-
-function updateLibrary () {
-    for(let i = 0; i <= myLibrary.length; i++) {
-        
-    };
+function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookProgress) {
+    myLibrary.push(new Book(bookTitle, bookAuthor, bookPages, bookProgress));
+    console.log(myLibrary[0]);
 };
