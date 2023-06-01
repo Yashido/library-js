06be985cbfe5d2baa85449dace0ff31f9dea5489
Yashido <jbpablico27@gmail.com>
2023-06-01 17:16:07 -0600

    JS dialog close outisde modal

--- script.js
+++ script.js
@@ -51,14 +51,14 @@ cancelBtn.addEventListener("click", removeModal);
     
     }
 
-modalBtn.addEventListener("click", () => {
-    const dialogDimensions = modal.getBoundingClientRect()
+dialog.addEventListener("click", (e) => {
+    const dialogDimensions = dialog.getBoundingClientRect()
     if(
         e.clientX < dialogDimensions.left ||
         e.clientX > dialogDimensions.right ||
         e.clientY < dialogDimensions.top ||
         e.clientY > dialogDimensions.bottom
     ) {
-        modal.close()
+        dialog.close()
     }
 });
